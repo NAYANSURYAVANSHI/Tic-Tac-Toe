4c2372f563beb3c9e50d840958f5c088abdacea2
NAYANSURYAVANSHI <nayansuryavanshi70@gmail.com>
2025-08-15 14:50:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn =document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; //playerX, playerO
+let count = 0;
+// let arr = ["apple","banana","litchi"];// 1D array
+// let arr2 = [["apple","litchi"],["potato","mushroom"],["pants","shirts"]];// 2D array
+
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+const resetGame = () => {
+    turnO = true;
+    count = 0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+
+boxes.forEach((box)=>{
+    box.addEventListener("click", () => {
+        // console.log("box was clicked");
+        // box.innerText = "abcd";
+        if(turnO){//playerO
+            box.innerText = "O";
+            box.style.color = "#9E0059";
+            turnO = false;
+        }else{//playerX
+            box.innerText = "X";
+            box.style.color = "#390099"
+            turnO = true;
+        }
+        box.disabled = true;
+        count++;
+        checkWinner();
+    });
+});
+
+const disableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const checkWinner = () => {
+    for (let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                // console.log("winner", pos1Val);
+                showWinner(pos1Val);
+                return;
+            }
+        }
+        if (count === 9) {
+            msg.innerText = "😄 It's a Draw! 😄";
+            msgContainer.classList.remove("hide");
+            disableBoxes();
+        }
+    }
+}
+
+newGameBtn.addEventListener("click", resetGame)
+resetBtn.addEventListener("click", resetGame);
